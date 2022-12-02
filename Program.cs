9ea71b4e7287a917fbe1f83c23ff22506b435108
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-12-01 21:14:46 -0500

    created method to write employee data to a .csv file

--- Program.cs
+++ Program.cs
@@ -36,21 +36,12 @@ namespace CatWorx.BadgeMaker
             return employees;
         }
 
-        static void PrintEmployees(List<Employee> employees)
-        {
-            for (int i = 0; i < employees.Count; i++)
-            {
-                string template = "{0,-10}\t{1,-20}\t{2}";
-                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
-            }
-        }
-
         static void Main(string[] args)
         {
             // Code to get employees
             List<Employee> employees = GetEmployees();
-            PrintEmployees(employees);
-
+            Util.PrintEmployees(employees);
+            Util.MakeCSV(employees);
             
             
         }
