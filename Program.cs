77289e5dba7b99f5ffd8d6627c521775ffdc536e
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-12-09 00:32:25 -0500

    able to create employee badges with data the user inputs. Using api to fetch and create random users

--- Program.cs
+++ Program.cs
@@ -1,48 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-// using CatWorx.BadgeMaker;
+using System.Threading.Tasks;
 
 namespace CatWorx.BadgeMaker
 {
     class Program
     {
-        static List<Employee> GetEmployees()
-        {
-            List<Employee> employees = new List<Employee>();
-            // Collect user values until the value is an empty string
-            while (true)
-            {
-                Console.WriteLine("Please enter a name: (leave empty to exit ): ");
-                // Get a name from the console and assign it to a variable
-                string firstName = Console.ReadLine() ?? "";
-                // Break if the user hits ENTER without typing a name
-                if (firstName == "")
-                {
-                    break;
-                }
+        
 
-                // add a Console.ReadLine() for each value
-                Console.Write("Enter last name: ");
-                string lastName = Console.ReadLine() ?? "";
-                Console.Write("Enter ID: ");
-                int id = Int32.Parse(Console.ReadLine() ?? "");
-                Console.Write("Enter Photo URL: ");
-                string photoUrl = Console.ReadLine() ?? "";
-                
-                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-                employees.Add(currentEmployee);
-
-            }
-            return employees;
-        }
-
-        static void Main(string[] args)
+        async static  Task Main(string[] args)
         {
             // Code to get employees
-            List<Employee> employees = GetEmployees();
+            // List<Employee> employees = GetEmployees();
+            
+            List<Employee> employees = await PeopleFetcher.GetFromApi();
             Util.PrintEmployees(employees);
             Util.MakeCSV(employees);
-            Util.MakeBadges(employees);
+            await Util.MakeBadges(employees);
             
         }
     }
