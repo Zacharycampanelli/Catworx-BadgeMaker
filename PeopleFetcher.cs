54d3930ab7826601c06214dde36c9362b9d6a666
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-12-09 00:53:35 -0500

    able to generate random employee cards

--- PeopleFetcher.cs
+++ PeopleFetcher.cs
@@ -45,7 +45,20 @@ namespace CatWorx.BadgeMaker
             using (HttpClient client = new HttpClient())
             {
                 string response = await client.GetStringAsync("https://randomuser.me/api/?results=10&nat=us&inc=name,id,picture");
-                Console.Write(response);
+                JObject json = JObject.Parse(response);
+
+                foreach (JToken token in json.SelectToken("results"))
+                {
+                    // Parse JSON data
+                    Employee emp = new Employee
+                    (
+                        token.SelectToken("name.first")!.ToString(),
+                        token.SelectToken("name.last")!.ToString(),
+                        Int32.Parse(token.SelectToken("id.value")!.ToString().Replace("-", "")),
+                        token.SelectToken("picture.large")!.ToString()
+                    );
+                    employees.Add(emp);
+                }
             }
             return employees;
         }
